99b4eabaa8a48a277a32b41b4c73b38a785fe2e0
Song Young-jin <myevan80@gmail.com>
2014-03-03 16:50:39 +0900

    ! in project astylerc bug fix

--- AStyleFormat.py
+++ AStyleFormat.py
@@ -122,10 +122,8 @@ class AstyleformatCommand(sublime_plugin.TextCommand):
         # Expand environment variables first
         if path.startswith('./'):
              project_file_path = self.view.window().project_file_name()
-             print(project_file_path)
              project_dir_path = os.path.dirname(project_file_path)
-             path = project_dir_path + path[2:]
-             print(path)
+             path = project_dir_path + path[1:]
 
         fullpath = os.path.expandvars(path)
         if not os.path.exists(fullpath) or not os.path.isfile(fullpath):
